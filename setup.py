4dd19e9ab54904207f57ae603558313b9a0b20ed
Danny Dannaher <dandann@biosustain.dtu.dk>
2017-02-15 13:47:39 +0100

    Chore: Added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='ice',
+      version='0.0.1',
+      description='Ice communication',
+      author='Warlink',
+      author_email='danny.dannaher@gmail.com',
+      url='https://github.com/DD-DeCaF/ice-communication',
+      packages=find_packages(),
+     )
